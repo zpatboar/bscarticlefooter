e50a01f08529bf2b937e43aa30ae29065f1d1acc
zpatboar <zpbgh@zpb.co>
2015-08-24 22:51:54 -0500

    Initial commit
    This is the first commit of the plugin to GitHub. This will serve as a
    base vs the current system of keeping the .zip files which could prove
    troublesome.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # bscarticlefooter
+
+This Joomla! 2.5/3.x plugin is designed to provide a simple way to manage a site wide article footer. With the ability to select certain categories to show the footer on. 
+
+For more information see the [homepage](http://boarmanservicecompany.com/programs/article-footer-plugin)
---
+++ bscarticlefooter.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @copyright	Copyright (C) 2012-2013 Boarman Service Company, LLC. All rights reserved.
+ * @license	GNU General Public License version 2 or later;
+ */
+
+defined('_JEXEC') or die;
+
+jimport( 'joomla.plugin.plugin' );
+
+class plgContentBSCArticleFooter extends JPlugin
+{
+
+	public function onContentPrepare($context, &$article, &$params)
+	{
+                $option = JRequest::getCmd('option');
+                $view = JRequest::getCmd('view');
+                if ($option == 'com_content' && ( ( $context == 'com_content.article' && $view = 'article' && $this->isCategoryRight() ) OR ($this->params->get('frontpage') == 'yes' && $context == 'com_content.featured' && $view = 'featured') ) ){//without $context shows for mod_custom, doesn't hurt to check.
+        		// Get the footer
+        		$footer = $this->params->get('text');
+        		$footer = trim($footer);
+                        $article->text .= $footer;
+                }
+	}
+
+        private function isCategoryRight(){
+                $db = JFactory::getDBO();
+                 $db->setQuery('SELECT catid FROM #__content WHERE id = '.JRequest::getInt('id'));
+                 $catid = $db->loadResult();
+                 $categories = (array) $this->params->get('category');
+                 if ( in_array($catid,$categories) ){
+                        return true;
+                } else {
+                        return false;
+                }
+        }
+
+}
